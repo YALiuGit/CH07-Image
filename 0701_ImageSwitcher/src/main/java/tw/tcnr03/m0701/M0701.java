f796d9e27c8aec501a0e2fcc7d731f3177a46d87
YALiuGit <tobeornotbeisaquestion@gmail.com>
2021-12-07 16:52:53 +0800

    202112071651

--- 0701_ImageSwitcher/src/main/java/tw/tcnr03/m0701/M0701.java
+++ 0701_ImageSwitcher/src/main/java/tw/tcnr03/m0701/M0701.java
@@ -14,10 +14,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ViewSwitcher;
 
+//implements增加介面ViewFactory及OnItemClickListener
 public class M0701 extends AppCompatActivity implements
         ViewSwitcher.ViewFactory,
         AdapterView.OnItemClickListener {
-    // 圖庫的圖片資源索引
+    // 圖庫的圖片索引
     private Integer[] imgArr = {
             R.drawable.img01, R.drawable.img02,
             R.drawable.img03, R.drawable.img04,
@@ -28,7 +29,7 @@ public class M0701 extends AppCompatActivity implements
             R.drawable.img13, R.drawable.img14,
             R.drawable.img15, R.drawable.img16,
     };
-
+//圖庫的縮圖索引
     private Integer[] thumbImgArr = {
             R.drawable.img01th, R.drawable.img02th,
             R.drawable.img03th, R.drawable.img04th,
@@ -58,7 +59,7 @@ public class M0701 extends AppCompatActivity implements
     }
 
     private void setupViewComponet() {
-        // 從資源類別R中取得介面元件
+        // 取得GridView元件
         gridview = (GridView) findViewById(R.id.m0701_g001);
         // 從資源類別R中取得介面元件
         imgSwi = (ImageSwitcher) findViewById(R.id.m0701_im01);
